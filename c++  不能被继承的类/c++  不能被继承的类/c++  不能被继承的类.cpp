4b2778cafc3358a23fa1ba4f8cf977b39761d07b
CamphorMiss <1963083540@qq.com>
2019-05-15 20:35:53 +0800

    设计一个不能被继承的类

---
+++ c++  不能被继承的类/c++  不能被继承的类/c++  不能被继承的类.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+class A//C++98中的方法，把基类的构造函数给成私有的，派生类调用不辽它，无法继承
+{
+public:
+	static A GetA()//静态函数，不然只能作为类的成员函数调用，则必须要先有类的对象
+	{
+		return A();
+	}
+private:
+	A()
+	{
+		//此处构造函数
+	}
+	int num;
+};
+class B final//c++11中，用final防止类被继承
+{
+//......
+};
+int main()
+{
+
+	return 0;
+}
