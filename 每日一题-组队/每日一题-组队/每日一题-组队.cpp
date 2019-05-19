ef82faeab3881d59244d1d0c105130df3651122b
CamphorMiss <1963083540@qq.com>
2019-05-19 22:25:59 +0800

    组队竞赛，求平均水平只和

---
+++ 每日一题-组队/每日一题-组队/每日一题-组队.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	int i = 0;
+	int a_i;
+	long long num = 0;
+	cin >> n;
+	vector<int> arr;
+	for (i = 0; i < 3 * n; i++)
+	{
+		cin >> a_i;
+		arr.push_back(a_i);
+
+	}
+	sort(arr.begin(), arr.end());
+	for (i = 0; i < n; i++)
+	{
+		num += arr[i * 2 + n];
+	}
+	cout << num << endl;
+	return 0;
+}
+
+
