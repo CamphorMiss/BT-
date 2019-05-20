0edcf2c975d21551bc6df8a5119e39da167d934b
CamphorMiss <1963083540@qq.com>
2019-05-20 23:00:03 +0800

    string C++  逆置字符串

---
+++ 每日练习-逆置字符串/每日练习-逆置字符串/每日练习-逆置字符串.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+//int main() {
+//	string str;
+//	while (getline(cin, str)) {
+//		reverse(str.begin(), str.end());
+//		int i = 0, j = i;
+//		while (i < str.size()) {
+//			while (i < str.size() && str[i] == ' ')
+//				++i;
+//			j = i;
+//			while (i < str.size() && str[i] != ' ')
+//				++i;
+//			reverse(str.begin() + j, str.begin() + i);
+//			j = i;
+//		}
+//		cout << str << endl;
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	string str;
+	int i=0;
+	int j = 0;
+	getline(cin,str);
+	int pos=0;
+
+	reverse(str.begin(),str.end());
+
+	while(i<str.size())
+	{
+		while(i<str.size()&&str[i]==' ')
+		{
+			++i;
+			j = i;
+		}
+		while (i < str.size() && str[i] != ' ')
+		{
+			++i;
+			
+		}
+		reverse(str.begin()+j,str.begin()+i);
+	}
+	cout<<str<<endl;
+	return 0;
+}
