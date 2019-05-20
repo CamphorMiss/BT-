7db0e7dd6f92224b091ce2dfc0e5f62943eae545
CamphorMiss <1963083540@qq.com>
2019-05-20 23:01:49 +0800

    C++   string  删除公共字符串

---
+++ 每日习题-删除公共字符/每日习题-删除公共字符/每日习题-删除公共字符.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	string longstr;
+	string shortstr;
+	getline(cin,longstr);
+	getline(cin,shortstr);
+	for (auto i = longstr.begin(); i != longstr.end(); ++i)
+	{
+		auto ret = find(shortstr.begin(), shortstr.end(), (*i));
+		if (ret != shortstr.end())
+		{
+			longstr.erase(i);
+			--i;
+		}
+	}
+	cout << longstr << endl;
+	return 0;
+}
