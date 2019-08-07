ee1fed3dcb96719c9c66328f1d8a99a6e910a4ff
CamphorMiss <1963083540@qq.com>
2019-08-07 18:20:19 +0800

    回家第二天，加油

---
+++ 19-3-16/19-3-16/test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+namespace zd
+{
+	/*int a=20;
+	int b=10;*/
+	int a;
+	int b;
+	cin>>a;
+	cin>>b;
+	int Add(int left,int right)
+	{
+		return left+right;
+	}
+}
+
+int main()
+{
+	/*int a=30;
+	int b=15;
+	printf("%d\n",a);
+	printf("%d\n",zd::a);*/
+	int a,b;
+	cin>>a;
+	cin>>b;
+	cout<<a<<"\n"<<b<<endl;
+
+	system("pause");
+	return 0;
+
+}
