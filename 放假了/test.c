1e838a69a70baee60fc4efca9daed74dc9ee135b
CamphorMiss <1963083540@qq.com>
2019-08-23 13:38:47 +0800

    emmmm

---
+++ 放假了/test.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define  F(x,y) ((x)+(y))
+enum E
+{
+a,
+b,
+c=5,
+d,
+e
+};
+int main()
+{
+//int a=3,b=4;
+//printf("%d\n",F(a++,b++));
+	/*int x;
+	x=5>1+2&&2||2*4<4-!0;
+	printf("%d\n",x);*/
+	/*int i=1,sum=0;
+	while(i<10)
+	{
+	   sum=sum+1;
+	   i++;
+	
+	}
+	printf("i=%d,sum=%d\n",i,sum);*/
+	/*long i=9;
+	printf("%d",sizeof(i));*/
+
+printf("%d\n",d);
+system("pause");
+return 0;
+}
