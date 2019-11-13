f86caa3a211c5c76df84f60aa4a886dc9bb7fbe1
CamphorMiss <1963083540@qq.com>
2019-11-13 23:39:00 +0800

    脑子被僵尸吃掉了

---
+++ 斐波那契数列新get方法/斐波那契数列新get方法/test.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int Fibonacci(int n) {
+        int a=0,b=1;
+        while((n--)>0)
+        {
+            b=a+b;
+            a=b-a;//Ô­À´µÄb
+        }
+        return a;
+    }
+};
